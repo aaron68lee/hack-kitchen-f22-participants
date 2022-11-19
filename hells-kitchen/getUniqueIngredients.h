61cdc2df10d61bbffd6895ea6291a238e1bfc4f4
aaron68lee <aaron68lee@gmail.com>
2022-11-19 15:40:14 -0800

    phase 1

--- hells-kitchen/getUniqueIngredients.h
+++ hells-kitchen/getUniqueIngredients.h
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+#include <unordered_set>
+
 using namespace std;
 
+string process(string s)
+{
+    string str = "";
+    char c;
+    for(int i = 0; i < s.length(); i++)
+    {
+        c = s[i];
+        if (isalpha(c)) // remove space and digits
+        {
+            str += tolower(c);
+        }
+    }
+    return str;
+}
+
 int getUniqueIngredients(string ingredients[], int size) {
-    return 0;
+    //ignore digits, spaces, symbols, and case    
+    unordered_set <string> set;
+    string temp;
+    for (int i = 0; i < size; i++) {
+        temp = process(ingredients[i]);
+        set.insert(temp);
+    }
+    return set.size();
 }

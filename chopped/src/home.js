9d38ec52b8528d9eed0e984024515e86f12d344b
Kenzie MacDougal <kmacd02@g.ucla.edu>
2022-11-19 12:04:05 -0800

    Create assets folder and assets

---
+++ chopped/src/home.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function Home() {
+    return (
+      <div>
+        <h1> Kenzie's Banana Pancakes </h1>
+        <img src="assets/pancakes.jpg" alt="Image of Banana Pancakes"> </img>
+        <body>
+            <h2> Steps: </h2>
+            <p> 1. In a large bowl, peel and mash all bananas.
+                
+            </p>
+        </body>
+      </div>
+
+    );
+  }
+  
+  export default Home;

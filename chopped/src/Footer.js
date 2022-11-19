ff0b755391305b7f44581699ba4dafa652586e37
aaron68lee <aaron68lee@gmail.com>
2022-11-19 12:40:49 -0800

    more app js stuff

--- chopped/src/Footer.js
+++ chopped/src/Footer.js
@@ -4,12 +4,13 @@ import React from "react";
 function Footer()
 {
     return(
-        <div>
-
+        <div class="footer">
         
             <footer>
             <p>Website by: Aaron Lee, Zain Bustani, Kenzie Macdougal, Michael Jiang</p>
-            <p><a href="https://www.allrecipes.com/recipe/20334/banana-pancakes-i/">Recipe_for_banana_pancakes</a></p>
+            <p>
+                <a href="https://www.allrecipes.com/recipe/20334/banana-pancakes-i/">Recipe_for_banana_pancakes</a>
+            </p>
             </footer>
 
         </div>
